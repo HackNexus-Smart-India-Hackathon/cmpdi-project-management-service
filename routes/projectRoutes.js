79c9df772036dd3f62424b3bfab79363866f54de
hustlernik <hustlernikxd@gmail.com>
2024-12-05 19:35:33 +0530

    api routes

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createProject,
   getProjectsByAdmin,
+  getInvestigatorsByProjectId,
 } from "../controllers/projectContoller.js";
 
 const router = express.Router();
 
 router.post("/create", createProject);
 router.get("/admin/:adminId/projects", getProjectsByAdmin);
+router.get("/project/:projectId/investigators", getInvestigatorsByProjectId);
 
 export default router;
